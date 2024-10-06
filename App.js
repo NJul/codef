994424986271010fe91f1458b9d78c1b8b2a4274
Nina NJul <nina.dev.web@gmail.com>
2024-10-06 18:23:00 +0300

    feat: add StyleSheet

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>The first line of text in the app.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, StyleSheet!</Text>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightblue',
+  },
+  text: {
+    fontSize: 24,
+    color: 'firebrick',
+  },
+});
